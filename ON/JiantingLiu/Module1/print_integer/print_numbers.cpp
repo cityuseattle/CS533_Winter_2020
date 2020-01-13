886a4075127d26eda38263d8a20d032f21402826
GTian4MSM <46945961+GTian4MSM@users.noreply.github.com>
2020-01-12 23:00:36 -0800

    Submission for module 1

---
+++ ON/JiantingLiu/Module1/print_integer/print_numbers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=17;
+    int y=23;
+    int z=56;
+
+    cout<<x<<"\n"<<y<<"\n"<<z<<endl;
+}
---
+++ ON/JiantingLiu/Module1/print_string/print_two_strings.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"This is string one"<<endl;
+    cout<<"This is string two";
+
+    return 0;
+}
