6a084df92025426d6ea3bc29747cc90c2c3c40fb
minhtruong1808 <zany.cobbler1808@gmail.com>
2020-01-13 08:06:48 -0800

    Submission for Module 1

---
+++ ON/MinhNguyen/Module1/print-integer-value/print-numbers.s
@@ -0,0 +1,28 @@
+    .data
+n1: .asciiz "\n"
+    .text
+
+
+main:
+        li $v0, 1
+        li $a0, 17
+        syscall
+
+        li $v0, 4
+        la $a0,n1
+        syscall
+
+        li $v0, 1
+        li $a0, 71
+        syscall
+
+        li $v0, 4
+        la $a0,n1
+        syscall
+
+        li $v0, 1
+        li $a0, 45
+        syscall
+
+        li  $v0, 10
+        syscall
