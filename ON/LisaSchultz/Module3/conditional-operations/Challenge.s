93ff76779b4289e731f93fcdd0379b26c777d9bd
lrschultz <lisa.r.schultz5122@gmail.com>
2020-01-20 09:37:02 -0800

    Fixed comment in Challenge file

--- ON/LisaSchultz/Module3/conditional-operations/Challenge.s
+++ ON/LisaSchultz/Module3/conditional-operations/Challenge.s
@@ -2,7 +2,7 @@ main:
             li      $t0, 2          # t0=2
             li      $t1, 5          # t1=5
 
-            bge		$t0, $t1, end_if	# if $t0 >= $t1 then branch to else_statm
+            bge		$t0, $t1, end_if	# if $t0 >= $t1 then branch to end_if
             
             add		$a0, $t0, $t1		    # $a0 = $t0 + $t1
             li      $v0, 1                  # print int		
