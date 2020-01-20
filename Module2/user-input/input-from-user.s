5c7fd8cb0d10cd785fdd99735d4588b659bd5b10
Shang Wu <wushangfinn@gmail.com>
2020-01-20 03:42:27 -0800

    Submission for Module2

---
+++ Module2/user-input/input-from-user.s
@@ -0,0 +1,34 @@
+        .data
+prompt: .asciiz "Enter an interger: "
+respon: .asciiz "You entered "
+endl:   .asciiz "\n"
+        .text
+
+
+
+
+
+
+main:
+        li     $v0, 4
+        la     $a0, prompt
+        syscall
+
+        li     $v0, 5
+        syscall
+        move   $t0, $v0
+
+        li     $v0, 4
+        la     $a0, respon
+        syscall
+
+        li     $v0, 1
+        move   $t0, $t0
+        syscall
+
+        li     $v0, 4
+        la     $a0, endl
+        syscall
+
+        li     $v0, 10
+        syscall
