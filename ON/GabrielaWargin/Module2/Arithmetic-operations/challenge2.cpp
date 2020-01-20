4cae4c8477a8ff99724b76eadb6e5b84e29abcff
Gabby W <40610520+gabbywargin@users.noreply.github.com>
2020-01-19 16:41:55 -0800

    Submission for Module 2

---
+++ ON/GabrielaWargin/Module2/Arithmetic-operations/challenge2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t1 = 30;
+    int t2 = 5;
+    int sumNum;
+    int divNum;
+    int mulNum;
+
+    sumNum= t1+t2+7;
+    divNum= t1/t2;
+    mulNum= t1 * t2;
+
+    cout<< "sum of numbers:" << sumNum << "\ndiv of numbers is: " << divNum << "\nmul of numbers is: " <<mulNum;
+}
---
+++ ON/GabrielaWargin/Module2/user-input/input-from-user.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x; 
+    cout << "Enter an interger\n";
+    cin>>x;
+    cout << "You entered: "<< x;
+ 
+}
