1797e4d4c9f487c52cb58bca6fda0151ed39afd7
Shang Wu <wushangfinn@gmail.com>
2020-01-20 03:42:27 -0800

    Submission for Module2

---
+++ Module2/Arithmetic-operations/mulNums.s
@@ -0,0 +1,20 @@
+        .data
+msg1:   .asciiz "Mult result is "
+        .text
+main:
+        li		$t1, 30	
+        li      $t2, 5
+
+        mult     $t1, $t2
+        mflo    $t3
+
+        li      $v0, 4
+        la      $a0, msg1
+        syscall
+
+        li      $v0, 4
+        la      $a0, $t3
+        syscall
+
+        li      $v0, 10
+        syscall
