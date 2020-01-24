154d750aa9e216329260ec679dc38740e10b2d6c
unknown <cudebecjustin@cityuniversity.edu>
2020-01-24 12:33:37 -0800

    Submission for Module 3

---
+++ ON/JustinCudebec/Module3/conditional-operations/if-elseCondition-ble.s
@@ -0,0 +1,30 @@
+main: 
+        li  $t0, 1                      # t0=1
+        li  $t1, 0                      # t1=0
+
+        .data
+g:      .asciiz "Greater \n "
+ng:     .asciiz "Not Greater \n"
+hello:  .asciiz "Hello again \n"
+        .text
+
+        ble     $t0, $t1, else_statm    # This condition is true because t0=t1, so it will jump to else_statm label
+
+
+        li      $v0, 4
+        la      $a0, g 
+        syscall
+        j end_if
+
+else_statm:                             # It will jump here because t0 is not > t1
+
+        li      $v0, 4
+        la      $a0, ng                 # load the address of ng string in a0
+        syscall                         # print the string in a0
+end_if:
+        li      $v0, 4 
+        la      $a0, hello              # print "Hello again"
+        syscall
+
+        li      $v0, 10
+        syscall
---
+++ ON/JustinCudebec/Module3/conditional-operations/if-elseCondtion-bgt.s
@@ -0,0 +1,29 @@
+main:
+        li  $t0, 0                      #t0=0
+        li  $t1, 0                      #t1=0
+
+        .data
+if:     .asciiz "If \n "
+else:   .asciiz "Else \n"
+hello:  .asciiz "Hello again \n"
+        .text
+
+        bgt     $t0, $t1, else_statm    # condition is true if t0 bigger than t1, so it will jump to
+                                        # the "else-statement" label
+
+        li      $v0, 4
+        la      $a0, if
+        syscall
+        j end_if
+
+else_statm: 
+        li      $v0, 4
+        la      $a0, else
+        syscall
+end_if:
+        li      $v0, 4
+        la      $a0, hello
+        syscall
+
+        li $v0, 10
+        syscall
