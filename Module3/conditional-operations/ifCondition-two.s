aab4a769342c1e80991744b208658d869dda4a54
Shang Wu <wushangfinn@gmail.com>
2020-01-30 00:57:54 -0800

    Submission for Module3

---
+++ Module3/conditional-operations/ifCondition-two.s
@@ -0,0 +1,24 @@
+main:
+        li		$t0,0
+        li      $t1,0
+
+        .data
+hello:  .asciiz"Hello"
+world:  .asciiz"World \n"
+        .text
+
+        bne     $t0, $t1, end_if
+
+
+        li      $v0, 4
+        la      $a0, hello
+        syscall
+
+end_if:
+
+        li      $v0,4
+        la      $a0, world
+        syscall
+        
+        li      $v0,10
+        syscall
