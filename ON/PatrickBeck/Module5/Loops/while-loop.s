eedf6e1e14201543c1e9288c0a35ab3aa4b5fa74
Patrick J Beck <beckpatrick@cityuniversity.edu>
2020-02-03 11:32:49 -0800

    Submission for Module 5

---
+++ ON/PatrickBeck/Module5/Loops/while-loop.s
@@ -0,0 +1,29 @@
+        .data
+num:    .asciiz "please enter a number: "
+Thanku: .asciiz "Thank you"
+        .text
+
+main:
+        li      $t0, 5          # t0=5  this is the number that we want the user to guess
+
+while:
+        beq     $t0, $t1,exitt  # if condition true t0=t1, jump to exit. Otherwise, it will execute while loop
+
+        li      $v0, 4
+        la      $a0, num        # please enter a number
+        syscall
+
+        li      $v0, 5
+        syscall                 # Read input from user
+        move    $t1, $v0        # $t1 = v0 save teh input in t1
+
+        j       while           # jump to while label to check the condition
+
+exitt:                          # end of loop
+
+        li      $v0, 4
+        la      $a0, Thanku     # print thank you
+        syscall
+
+        li      $v0, 10
+        syscall
