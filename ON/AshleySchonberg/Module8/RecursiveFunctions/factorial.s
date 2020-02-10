246df137ea34c7ffe4196aab83a029f53366524c
ashjayne <ashjayne@users.noreply.github.com>
2020-02-09 20:14:35 -0800

    Submission for Module 8

---
+++ ON/AshleySchonberg/Module8/RecursiveFunctions/factorial.s
@@ -0,0 +1,55 @@
+.data
+    prompt: .asciiz "Input an integer x:\n"
+    result: .asciiz "Fact(x) = "
+.text
+
+factorial:
+    # base case -- still in parent's stack segment
+    # adjust stack pointer to store return address & argument
+    addi	$sp, $sp, -8
+    # save $s0 and $ra
+    sw		$s0, 4($sp)
+    sw      $ra, 0($sp)
+    bne     $a0, 0, else
+    addi    $v0, $zero, 1           # return 1
+    j		fact_return				# jump to fact_return
+
+else:
+    # backup $a0
+    move 	$s0, $a0
+    addi    $a0, $a0, -1            # x -= 1
+    jal		factorial				# jump to factorial and save position to $ra
+    # when we get here we already have Fact(x-1) stored in $v0
+    multu	$s0, $v0			    # return x*Fact(x-1)
+    mflo    $v0
+
+fact_return:
+    lw		$s0, 4($sp)
+    lw      $ra, 0($sp)
+    addi    $sp, $sp, 8
+    jr		$ra					    # jump to $ra
+
+main:
+    # show prompt
+    li		$v0, 4
+    la		$a0, prompt
+    syscall
+    # read x
+    li      $v0, 5
+    syscall
+    # function call
+    move    $a0, $v0
+    jal     factorial
+    move    $t0, $v0
+    # show prompt
+    li      $v0, 4
+    la      $a0, result
+    syscall
+    # print the result
+    li      $v0, 1
+    move    $a0, $t0
+    syscall
+    
+    li      $v0, 10
+    syscall
+    
