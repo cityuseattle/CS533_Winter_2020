b15d1737ef05f776a92abde4c2383aa2b060c950
Patrick J Beck <beckpatrick@cityuniversity.edu>
2020-02-21 11:31:21 -0800

    Submission for Module 10

---
+++ ON/PatrickBeck/Module10/logiclOperations/immediateOperation.s
@@ -0,0 +1,41 @@
+                    .data
+n1:                 .asciiz "\n"
+                    .text
+
+main:
+
+                    li        $t0, 7                        #binary equivalent of 7 = 111
+
+                    andi      $t1, $t0, 5                   # 111 and 101 = 101 = 5 in decimal
+
+                    li        $v0, 1
+                    move      $a0, $t1                      # load t1 into a0 to be printed
+                    syscall
+
+                    li        $v0, 4
+                    la        $a0, n1                       # load the address of n1 into a0
+                    syscall
+
+                    
+                    xori      $t1, $t0, 5                   # 111 xor 101 = 111 = 2 in decimal
+
+                    li        $v0, 1
+                    move      $a0, $t1                      # load t1 into a0 to be printed
+                    syscall 
+
+                    li        $v0, 4
+                    la        $a0, n1                       # load the address of n1 into a0
+                    syscall
+
+                    ori       $t1, $t0, 5                   # 111 or 101 = 111 = 7 in decimal
+
+                    li        $v0, 1
+                    move      $a0, $t1                      # load t1 into a0 to be printed
+                    syscall
+
+                    li        $v0, 4
+                    la        $a0, n1                       # load the address of n1 into a0
+                    syscall 
+
+                    li        $v0, 10
+                    syscall 
---
+++ ON/PatrickBeck/Module10/logiclOperations/shiftleftOperation.s
@@ -0,0 +1,11 @@
+main:
+          li        $t1, 7              #111
+
+          sll       $t2, $t1, 1         #shift by 1, t2 = 1110 which equal to 14 in decimal
+
+          li        $v0, 1
+          move      $a0, $t2
+          syscall
+
+          li        $v0, 10
+          syscall 
