58b2f2ee533dedac387e3d9ef986aa8973712155
minhtruong1808 <zany.cobbler1808@gmail.com>
2020-03-03 20:36:08 -0800

    Submission for Module 10

---
+++ ON/MinhNguyen/Module10/logicalOperations/orOperations.s
@@ -0,0 +1,14 @@
+main:
+
+    li  $t1, 7              #binary equivalent of 7 = 111
+    li  $t2, 5              # 5 = 101
+
+
+    or $t0, $t1, $t2       # 111 or 101 = 111 = 7 in decimal
+
+    li  $v0, 1
+    move $a0, $t0 
+    syscall
+
+    li  $v0, 10
+    syscall
