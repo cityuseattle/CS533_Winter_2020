dfeaa92e0f4498b21f4a1c2a41d52b69b019eaf7
karnatisravya <karnatisravya@cityuniversity.edu>
2020-03-09 22:47:00 -0700

    submitting HOP 09,submitting HOP 10

---
+++ IN/SravyaKarnati/Module10/logiclOperations/ rorOperation.s
@@ -0,0 +1,9 @@
+main: 
+
+li  $t1, 8
+
+ror  $t2, $t1, 1
+
+li   $v0, 1
+move $a0, $t2
+syscall
---
+++ IN/SravyaKarnati/Module10/logiclOperations/shiftleftOperation.s
@@ -0,0 +1,12 @@
+main:
+
+li  $t1, 7
+
+s11 $t2, $t1, 1
+
+li    $v0, 1
+move  $a0, $t2
+syscall
+
+li   $v0, 10
+syscall
