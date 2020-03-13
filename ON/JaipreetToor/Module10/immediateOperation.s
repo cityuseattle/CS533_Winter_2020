cb6b74358dcd6a336dfaf717ae749121f084cec9
Jaipreet Toor <jaipreet.toor@gmail.com>
2020-03-13 00:23:56 -0700

    Submission for Module 10

---
+++ ON/JaipreetToor/Module10/immediateOperation.s
@@ -0,0 +1,38 @@
+data
+n1: .asciiz "\n"
+    .text
+
+main:
+    li      $t0, 7             
+    andi    $t1, $t0, 5        
+
+    li      $v0, 1
+    move    $a0, $t1            
+    syscall
+
+    li      $v0, 4
+    la      $a0, n1             
+    syscall
+
+    xori    $t1, $t0, 5        
+
+    li      $v0, 1
+    move    $a0, $t1           
+    syscall
+
+    li      $v0, 4
+    la      $a0, n1          
+    syscall
+
+    ori     $t1, $t0, 5    
+
+    li      $v0, 1
+    move    $a0, $t1       
+    syscall
+
+    li      $v0, 4
+    la      $a0, n1   
+    syscall
+
+    li      $v0, 10
+    syscall 
