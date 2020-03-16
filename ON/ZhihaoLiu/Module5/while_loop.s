a40f54c1d633fd64c6e64ecb603aefef921d2f6f
qwyp003 <qwyp003@gmail.com>
2020-03-15 19:02:30 -0700

    Submission for Module 5

---
+++ ON/ZhihaoLiu/Module5/while_loop.s
@@ -0,0 +1,28 @@
+        .data
+num:    .asciiz "please enter a number: "
+Thanku: .asciiz "Thank you"
+        .text
+
+main:
+        li		$t0, 5		
+while:
+        beq		$t0, $t1, exitt	
+        
+        li      $v0, 4
+        la		$a0, num		
+        syscall
+
+        li      $v0,5  
+        syscall
+        move 	$t1, $v0		
+        
+        j		while				
+
+exitt:
+
+        li      $v0, 4
+        la      $a0, Thanku
+        syscall
+
+        li      $v0, 10
+        syscall
